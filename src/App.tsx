1dd814140864196ce1cc7b514c0e06418209f8c9
KingNono1030 <hoejy0821@gmail.com>
2025-02-03 13:28:09 +0900

    remove: 구글 프로바이더 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Suspense } from 'react';
 
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter } from 'react-router-dom';
 
-import { GOOGLE_CLIENT_ID } from './constants/api/oauth';
 import ScrollToTop from './layout/ScrollToTop';
 import Router from './routes';
 
@@ -15,16 +13,13 @@ const queryClient = new QueryClient();
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-        <BrowserRouter>
-          <Suspense>
-            <ScrollToTop />
-            <Router />
-          </Suspense>
-        </BrowserRouter>
-        {/* The rest of your application */}
-        <ReactQueryDevtools initialIsOpen={false} />
-      </GoogleOAuthProvider>
+      <BrowserRouter>
+        <Suspense>
+          <ScrollToTop />
+          <Router />
+        </Suspense>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
