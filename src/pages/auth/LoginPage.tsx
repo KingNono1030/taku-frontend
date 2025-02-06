f58c4b9a5076eb0ecb1841510acf45e8e222b9e1
KingNono1030 <hoejy0821@gmail.com>
2025-02-06 23:29:22 +0900

    remove: 테스트용 버튼 제거

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -7,7 +7,7 @@ import { GOOGLE_OAUTH_URI, KAKAO_OAUTH_URI } from '@/constants/api/oauth';
 
 const LoginPage = () => {
   return (
-    <div className="mx-auto mb-[6.25rem] mt-[3.75rem] w-[560px]">
+    <div className="mx-auto my-[3.75rem] w-[560px]">
       <h1 className="mb-8 text-[2rem] font-semibold leading-[2.625rem]">
         로그인
       </h1>
@@ -30,24 +30,6 @@ const LoginPage = () => {
               alt="구글 로그인 버튼"
             />
             Google 계정으로 로그인
-          </Button>
-        </Link>
-        <Link
-          to={
-            '/oauth/callback?token=eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjIsIm5pY2tuYW1lIjoibG9vY28iLCJwcm92aWRlclR5cGUiOiJLQUtBTyIsInByb2ZpbGVJbWciOiJodHRwczovL2R1Y2t3aG8tdmlkZW8uY2U0MDBhY2YwYThlYzg3MjY1YzhmZGE2ZWM2OGY5NTkucjIuY2xvdWRmbGFyZXN0b3JhZ2UuY29tLzk3OWRlZmRiLTJjZWQtNGM1NS1iNzY3LTA1NjdkYTQ0NjRmZi5wbmciLCJzdGF0dXMiOiJBQ1RJVkUiLCJkb21lc3RpY0lkIjoiMzgzNTM0MTc3NiIsImdlbmRlciI6Ik1BTEUiLCJhZ2VSYW5nZSI6IjIwfjI5Iiwicm9sZSI6IlVTRVIiLCJlbWFpbCI6InNlczk4OTJAbmF2ZXIuY29tIiwicG9zdHMiOltdLCJ0eXBlIjoiQUNDRVNTIiwiaWF0IjoxNzM2MjQwMDM3LCJleHAiOjE3Mzg4MzIwMzd9.d-mA2t2m_SDi6kyKMb-UmM2gwhEZdJ-IA0RcqTz3uhE&user={"nickname":"nono","profileImg":"https://picsum.photos/200/300","gender":"male","ageRange":"20~19"}'
-          }
-        >
-          <Button className="relative h-10 w-full border border-solid border-[#e5e7eb] bg-white py-4 font-semibold text-[#000000/85] hover:bg-white">
-            로그인 테스트
-          </Button>
-        </Link>
-        <Link
-          to={
-            '/register?token=eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjIsIm5pY2tuYW1lIjoibG9vY28iLCJwcm92aWRlclR5cGUiOiJLQUtBTyIsInByb2ZpbGVJbWciOiJodHRwczovL2R1Y2t3aG8tdmlkZW8uY2U0MDBhY2YwYThlYzg3MjY1YzhmZGE2ZWM2OGY5NTkucjIuY2xvdWRmbGFyZXN0b3JhZ2UuY29tLzk3OWRlZmRiLTJjZWQtNGM1NS1iNzY3LTA1NjdkYTQ0NjRmZi5wbmciLCJzdGF0dXMiOiJBQ1RJVkUiLCJkb21lc3RpY0lkIjoiMzgzNTM0MTc3NiIsImdlbmRlciI6Ik1BTEUiLCJhZ2VSYW5nZSI6IjIwfjI5Iiwicm9sZSI6IlVTRVIiLCJlbWFpbCI6InNlczk4OTJAbmF2ZXIuY29tIiwicG9zdHMiOltdLCJ0eXBlIjoiQUNDRVNTIiwiaWF0IjoxNzM2MjQwMDM3LCJleHAiOjE3Mzg4MzIwMzd9.d-mA2t2m_SDi6kyKMb-UmM2gwhEZdJ-IA0RcqTz3uhE&provider=KAKAO'
-          }
-        >
-          <Button className="relative h-10 w-full border border-solid border-[#e5e7eb] bg-white py-4 font-semibold text-[#000000/85] hover:bg-white">
-            유저등록 테스트
           </Button>
         </Link>
       </div>
