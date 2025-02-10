abd27e52eb70be0ac1608c836cee958095aa7bf6
KingNono1030 <hoejy0821@gmail.com>
2025-02-10 12:10:39 +0900

    feat: 장터 랭킹 상품 카드 컴포넌트 추가

---
+++ src/components/market/RankProductCard.tsx
@@ -0,0 +1,57 @@
+import { Link } from 'react-router-dom';
+
+import { formatCurrency } from '@/lib/utils';
+import { JangterRank } from '@/types/api/jangter.types';
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '../ui/card';
+
+export const RankProductCard = ({ data }: { data: JangterRank }) => {
+  const {
+    rank_idx,
+    product_id,
+    product_name,
+    product_image,
+    product_price,
+    author_name,
+  } = data;
+  return (
+    <Link to={`/market/${product_id}`}>
+      <Card className="relative overflow-hidden">
+        <div className="absolute left-0 top-0 z-10 flex h-[28px] w-[34px] items-center justify-center rounded-lg bg-black/50 text-xs text-white">
+          {rank_idx}
+        </div>
+        <div className="relative aspect-square w-full overflow-hidden">
+          {product_image && (
+            <img
+              className="absolute inset-0 w-full object-cover transition-transform duration-300 hover:scale-110"
+              src={product_image}
+              alt={`${product_name} 상품 썸네일`}
+            />
+          )}
+          {!product_image && (
+            <div className="flex aspect-square w-full items-center justify-center bg-gray-200">
+              등록된 상품 이미지가 없습니다...
+            </div>
+          )}
+        </div>
+        <CardHeader className="p-4">
+          <CardDescription>{product_name}</CardDescription>
+          <span className="text-gray-400">{author_name}</span>
+        </CardHeader>
+        <CardContent className="px-4 pb-4">
+          <CardTitle className="">
+            {formatCurrency(product_price as number)}
+          </CardTitle>
+        </CardContent>
+        <CardFooter className="flex flex-col items-start px-4 pb-4"></CardFooter>
+      </Card>
+    </Link>
+  );
+};
